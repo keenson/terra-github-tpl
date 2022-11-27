0e1705766512ac5eca142ef7aab87f6e21add0ee
Olalekan Fadele <os.fadele@gmail.com>
2022-11-27 04:29:35 +0100

    Added terraform bucket files

--- main.tf
+++ main.tf
@@ -15,16 +15,16 @@ provider "aws" {
 
 
 
-# resource "aws_s3_bucket" "b" {
-#   bucket = "my-tf-test-bucket"
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-test-bucket"
 
-#   tags = {
-#     Name        = "My bucket"
-#     Environment = "Dev"
-#   }
-# }
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
 
-# resource "aws_s3_bucket_acl" "example" {
-#   bucket = aws_s3_bucket.b.id
-#   acl    = "private"
-# }
+resource "aws_s3_bucket_acl" "example" {
+  bucket = aws_s3_bucket.b.id
+  acl    = "private"
+}
